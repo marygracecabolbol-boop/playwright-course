c6debd5970914d613a39bdb56c2805df33c83f54
marygracecabolbol-boop <marygracecabolbol@gmail.com>
2026-08-12 17:19:49 +0800

    deleting

--- Starter/playwright.config.ts
+++ Starter/playwright.config.ts
@@ -1,8 +1,23 @@
+// @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import { use } from 'express/lib/application';
 
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
 export default defineConfig({
   testDir: './tests',
   reporter: 'html',
+  fullyParallel: true,
+  workers: '75%',
   webServer: {
     command: 'npm start',
     url: 'http://localhost:3000/',
@@ -10,27 +25,32 @@ export default defineConfig({
   },
   use: {
     baseURL: 'http://localhost:3000/',
-    headless: false
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure'
   },
-  // projects: [
-  //   {
-  //     name: 'chromium',
-  //     use: { ...devices['Desktop Chrome'], },
-  //     dependencies:[
-  //       'auth-setup'
-  //     ]
-  //   },
-  //   {
-  //     name: 'firefox',
-  //     use: { ...devices['Desktop Firefox'] },
-  //     dependencies:[
-  //       'auth-setup'
-  //     ]
-  //   },
-  //   {
-  //     name: 'auth-setup',
-  //     testMatch: 'tests/setup/Auth.setup.ts',
-  //   }
-  // ]
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], },
+      dependencies: [
+        'auth-setup'
+      ]
+    },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox']},
+    //   dependencies: [
+    //     'auth-setup'
+    //   ]
+    // },
+    {
+      name: 'auth-setup',
+      testMatch: 'tests/setup/Auth.setup.ts',
+    }
+  ]
 
 });
+
